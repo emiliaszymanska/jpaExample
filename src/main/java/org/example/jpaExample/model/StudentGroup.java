8839816e690a1dea5047168694d691d6519bdc59
Emilia Szymanska <esz.emilia@gmail.com>
2020-11-09 21:54:40 +0100

    Add StudentGroup class

--- src/main/java/org/example/jpaExample/model/StudentGroup.java
+++ src/main/java/org/example/jpaExample/model/StudentGroup.java
@@ -1,18 +1,39 @@
 package org.example.jpaExample.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "Student_group")
 public class StudentGroup {
 
     private String name;
+
+    @OneToMany(mappedBy = "studentGroup", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
     private Set<Student> students = new HashSet<>();
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Enumerated(EnumType.STRING)
+    private CCLocation ccLocation;
 
     public StudentGroup() {
     }
 
-    public StudentGroup(String name) {
+    public StudentGroup(String name, CCLocation ccLocation) {
         this.name = name;
+        this.ccLocation = ccLocation;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
     }
 
     public String getName() {
